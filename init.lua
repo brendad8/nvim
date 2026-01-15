f6441cb5d1e5e7863b68d07fb5ff3393a220af72
Brendan Callender <72055001+brendad8@users.noreply.github.com>
2026-01-14 19:08:04 -0800

    first commit

---
+++ init.lua
@@ -0,0 +1,151 @@
+local vim = vim
+
+-- Globals
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Options
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.wrap = false
+vim.o.signcolumn = "yes"
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.expandtab = true
+vim.o.swapfile = false
+vim.o.winborder = "rounded"
+vim.o.clipboard = "unnamedplus"
+vim.o.completeopt = "menuone,noselect,popup"
+vim.o.pumheight = 8 -- max height for autocompletion menu
+
+
+-- Highlight text when copying
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+
+-- Keymaps
+vim.keymap.set("n", "<leader>so", ":update<CR> :source<CR>")
+vim.keymap.set("n", "<leader>w", ":write<CR>")
+vim.keymap.set("n", "<leader>qq", ":quit<CR>")
+
+vim.keymap.set("n", "<Tab>", ">>")
+vim.keymap.set("n", "<S-Tab>", "<<")
+
+vim.keymap.set("v", "<Tab>", ">gv")
+vim.keymap.set("v", "<S-Tab>", "<gv")
+
+vim.keymap.set("n", "<leader>bd", ":bd<CR>")
+vim.keymap.set("n", "<leader>bD", ":bd!<CR>")
+
+vim.keymap.set("n", "<leader>-", "<C-W>s")
+vim.keymap.set("n", "<leader>|", "<C-W>v")
+
+vim.keymap.set("n", "<leader>h", "<C-w>h")
+vim.keymap.set("n", "<leader>j", "<C-w>j")
+vim.keymap.set("n", "<leader>k", "<C-w>k")
+vim.keymap.set("n", "<leader>l", "<C-w>l")
+
+
+vim.keymap.set("i", "<Tab>", function()
+  if vim.fn.pumvisible() == 1 then
+    return "<C-n><C-y>"
+  else
+    return "<Tab>"
+  end
+end, { expr = true })
+
+-- Plugins
+vim.pack.add({
+  { src = "https://github.com/sainnhe/everforest" },
+  { src = "https://github.com/nvim-mini/mini.ai" },
+  { src = "https://github.com/nvim-mini/mini.comment" },
+  { src = "https://github.com/nvim-mini/mini.icons" },
+  { src = "https://github.com/nvim-mini/mini.pairs" },
+  { src = "https://github.com/nvim-mini/mini.pick" },
+  { src = "https://github.com/nvim-mini/mini.surround" },
+  { src = "https://github.com/nvim-mini/mini.tabline" },
+  { src = "https://github.com/stevearc/oil.nvim" },
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+})
+
+-- Colorsheme
+vim.cmd("colorscheme everforest")
+
+-- Mini Pick
+require("mini.pick").setup()
+vim.keymap.set("n", "<leader>ff", ":Pick files<CR>")
+vim.keymap.set("n", "<leader>fg", ":Pick grep_live<CR>")
+
+require("mini.ai").setup()
+require("mini.pairs").setup()
+require("mini.icons").setup()
+require("mini.surround").setup()
+require("mini.tabline").setup()
+
+-- Oil
+require("oil").setup()
+vim.keymap.set("n", "<leader>e", ":Oil<CR>")
+
+
+-- Enable Language Servers, Autocomplete, & Diagnostics
+vim.lsp.enable({"lua_ls", "ty", "ruff", "clangd"})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('my.lsp', {}),
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		if client:supports_method('textDocument/completion') then
+			local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+			client.server_capabilities.completionProvider.triggerCharacters = chars
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+		end
+	end,
+})
+
+vim.diagnostic.enable()
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
+
+
+
+
+
+
+-- Clean Unused Packages
+local function pack_clean()
+	local active_plugins = {}
+	local unused_plugins = {}
+
+	for _, plugin in ipairs(vim.pack.get()) do
+		active_plugins[plugin.spec.name] = plugin.active
+	end
+
+	for _, plugin in ipairs(vim.pack.get()) do
+		if not active_plugins[plugin.spec.name] then
+			table.insert(unused_plugins, plugin.spec.name)
+		end
+	end
+
+	if #unused_plugins == 0 then
+		print("No unused plugins.")
+		return
+	end
+
+	local choice = vim.fn.confirm("Remove unused plugins?", "&Yes\n&No", 2)
+	if choice == 1 then
+		vim.pack.del(unused_plugins)
+	end
+end
+
+vim.keymap.set("n", "<leader>pc", pack_clean)
